4a6bad209feba6647ac373356d51cdaa7ef0ab99
CobraGamingSj <shashikaanthjain.1983@gmail.com>
2024-10-17 19:54:09 +0530

    Recipe Issue.

--- src/main/java/net/moreores/block/entity/GemPolisherBlockEntity.java
+++ src/main/java/net/moreores/block/entity/GemPolisherBlockEntity.java
@@ -6,12 +6,18 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventories;
+import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
+import net.minecraft.recipe.RecipeEntry;
+import net.minecraft.recipe.RecipeManager;
+import net.minecraft.recipe.RecipeType;
+import net.minecraft.recipe.input.RecipeInput;
+import net.minecraft.recipe.input.SingleStackRecipeInput;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.screen.PropertyDelegate;
 import net.minecraft.screen.ScreenHandler;
@@ -22,29 +28,28 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.moreores.block.data.GemPolisherData;
 import net.moreores.item.ModItems;
+import net.moreores.recipe.GemPolisherRecipe;
 import net.moreores.screen.GemPolisherScreenHandler;
 import org.jetbrains.annotations.Nullable;
 
-public class GemPolisherBlockEntity extends BlockEntity implements ExtendedScreenHandlerFactory, ImplementedInventory {
-    private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(3, ItemStack.EMPTY); // Corrected size to 3
+import java.util.Objects;
+import java.util.Optional;
 
-    private static final int INPUT_SLOT = 0;
-    private static final int OUTPUT_SLOT = 1;
-    private static final int ENERGY_SLOT = 2;
+public class GemPolisherBlockEntity extends BlockEntity implements ExtendedScreenHandlerFactory, ImplementedInventory {
+    private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(15, ItemStack.EMPTY); // Corrected size to 15
+
+    public static final int INPUT_SLOT = 0;
+    public static final int OUTPUT_SLOT = 1;
+    public static final int ENERGY_SLOT = 2;
 
     protected final PropertyDelegate propertyDelegate;
     private int progress = 0;
     private int maxProgress = 400;
-
-//    public final SimpleEnergyStorage energyStorage = new SimpleEnergyStorage(100000, 32, 32) {
-//        @Override
-//        protected void onFinalCommit() {
-//            markDirty();
-//        }
-//    };
+    private final RecipeManager.MatchGetter<SingleStackRecipeInput, GemPolisherRecipe> matchGetter;
 
     public GemPolisherBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntityType.GEM_POLISHER_BLOCK_ENTITY, pos, state);
+        this.matchGetter = RecipeManager.createCachedMatchGetter(GemPolisherRecipe.Type.INSTANCE);
         this.propertyDelegate = new PropertyDelegate() {
             @Override
             public int get(int index) {
@@ -100,7 +105,6 @@ public class GemPolisherBlockEntity extends BlockEntity implements ExtendedScree
         super.readNbt(nbt, registryLookup);
         Inventories.readNbt(nbt, inventory, registryLookup);
         progress = nbt.getInt("gem_polisher.progress");
-//        energyStorage.amount = nbt.getLong("gem_polisher.energy");
     }
 
     @Override
@@ -108,12 +112,7 @@ public class GemPolisherBlockEntity extends BlockEntity implements ExtendedScree
         super.writeNbt(nbt, registryLookup);
         Inventories.writeNbt(nbt, inventory, registryLookup);
         nbt.putInt("gem_polisher.progress", progress);
-//        nbt.putLong("gem_polisher.energy", energyStorage.amount);
     }
-
-//    public void setEnergyLevel(long energyLevel) {
-//        this.energyStorage.amount = energyLevel;
-//    }
 
     @Override
     public Object getScreenOpeningData(ServerPlayerEntity player) {
@@ -141,16 +140,8 @@ public class GemPolisherBlockEntity extends BlockEntity implements ExtendedScree
             return;
         }
 
-//        if (hasEnergySource()) {
-//            try(Transaction transaction = Transaction.openOuter()) {
-//                energyStorage.insert(16, transaction);
-//                transaction.commit();
-//            }
-//        }
-
-        if (isOutputSlotEmptyOrReceivable() && hasRecipe() && hasEnergySource()) {
+        if (isOutputSlotEmptyOrReceivable() && hasRecipe()) {
             this.increaseCraftProgress();
-//            extractEnergy();
             if (hasCraftingFinished()) {
                 this.craftItem();
                 this.resetProgress();
@@ -162,68 +153,72 @@ public class GemPolisherBlockEntity extends BlockEntity implements ExtendedScree
         }
     }
 
-//    private void extractEnergy() {
-//        try(Transaction transaction = Transaction.openOuter()) {
-//            energyStorage.extract(32, transaction);
-//            transaction.commit();
-//        }
-//    }
-//
-//    private boolean hasEnoughEnergy() {
-//        return energyStorage.amount >= 32;
-//    }
-
-    private boolean hasEnergySource() {
-        return getStack(ENERGY_SLOT).getItem() == ModItems.ENERGY_INGOT;
-    }
-
     private void resetProgress() {
         this.progress = 0;
     }
 
     private void craftItem() {
-        ItemStack inputStack = getStack(INPUT_SLOT);
-        ItemStack result;
+        RecipeEntry<GemPolisherRecipe> recipe = getSlot1Recipe().orElseThrow();
+        ItemStack energySlot = getStack(ENERGY_SLOT);
 
-        if (inputStack.getItem() == ModItems.RAW_RUBY) {
-            result = new ItemStack(ModItems.RUBY);
-        } else if (inputStack.getItem() == ModItems.RAW_SAPPHIRE) {
-            result = new ItemStack(ModItems.SAPPHIRE);
-        } else if (inputStack.getItem() == ModItems.RAW_GREEN_SAPPHIRE) {
-            result = new ItemStack(ModItems.GREEN_SAPPHIRE);
-        } else if (inputStack.getItem() == ModItems.RAW_BLUE_GARNET) {
-            result = new ItemStack(ModItems.BLUE_GARNET);
-        } else if (inputStack.getItem() == ModItems.RAW_PINK_GARNET) {
-            result = new ItemStack(ModItems.PINK_GARNET);
-        } else if (inputStack.getItem() == ModItems.RAW_GREEN_GARNET) {
-            result = new ItemStack(ModItems.GREEN_GARNET);
-        } else if (inputStack.getItem() == ModItems.RAW_TOPAZ) {
-            result = new ItemStack(ModItems.TOPAZ);
-        } else if (inputStack.getItem() == ModItems.RAW_WHITE_TOPAZ) {
-            result = new ItemStack(ModItems.WHITE_TOPAZ);
-        } else if (inputStack.getItem() == ModItems.RAW_PERIDOT) {
-            result = new ItemStack(ModItems.PERIDOT);
-        } else if (inputStack.getItem() == ModItems.RAW_JADE) {
-            result = new ItemStack(ModItems.JADE);
-        } else if (inputStack.getItem() == ModItems.RAW_PYROPE) {
-            result = new ItemStack(ModItems.PYROPE);
-        } else {
-            return; // No valid input, so don't craft anything
-        }
 
         this.removeStack(INPUT_SLOT, 1);
-
-        ItemStack energyStack = getStack(ENERGY_SLOT);
-        if (energyStack.getItem() == ModItems.ENERGY_INGOT) {
-            if (energyStack.getDamage() < energyStack.getMaxDamage()) {
-                energyStack.setDamage(energyStack.getDamage() + 5);
-            } else {
-                this.removeStack(ENERGY_SLOT);
+        if (energySlot.getItem() == ModItems.ENERGY_INGOT) {
+            if (energySlot.getDamage() < energySlot.getMaxDamage()) {
+                energySlot.setDamage(energySlot.getDamage() + 5);
             }
+        }else {
+            this.removeStack(ENERGY_SLOT);
         }
 
-        this.setStack(OUTPUT_SLOT, new ItemStack(result.getItem(), getStack(OUTPUT_SLOT).getCount() + result.getCount()));
+
+        this.setStack(OUTPUT_SLOT, new ItemStack(recipe.value().getResult(null).getItem(),
+                getStack(OUTPUT_SLOT).getCount() + recipe.value().getResult(null).getCount()));
     }
+
+//    private void craftItem() {
+//        ItemStack inputStack = getStack(INPUT_SLOT);
+//        ItemStack result;
+//
+//        if (inputStack.getItem() == ModItems.RAW_RUBY) {
+//            result = new ItemStack(ModItems.RUBY);
+//        } else if (inputStack.getItem() == ModItems.RAW_SAPPHIRE) {
+//            result = new ItemStack(ModItems.SAPPHIRE);
+//        } else if (inputStack.getItem() == ModItems.RAW_GREEN_SAPPHIRE) {
+//            result = new ItemStack(ModItems.GREEN_SAPPHIRE);
+//        } else if (inputStack.getItem() == ModItems.RAW_BLUE_GARNET) {
+//            result = new ItemStack(ModItems.BLUE_GARNET);
+//        } else if (inputStack.getItem() == ModItems.RAW_PINK_GARNET) {
+//            result = new ItemStack(ModItems.PINK_GARNET);
+//        } else if (inputStack.getItem() == ModItems.RAW_GREEN_GARNET) {
+//            result = new ItemStack(ModItems.GREEN_GARNET);
+//        } else if (inputStack.getItem() == ModItems.RAW_TOPAZ) {
+//            result = new ItemStack(ModItems.TOPAZ);
+//        } else if (inputStack.getItem() == ModItems.RAW_WHITE_TOPAZ) {
+//            result = new ItemStack(ModItems.WHITE_TOPAZ);
+//        } else if (inputStack.getItem() == ModItems.RAW_PERIDOT) {
+//            result = new ItemStack(ModItems.PERIDOT);
+//        } else if (inputStack.getItem() == ModItems.RAW_JADE) {
+//            result = new ItemStack(ModItems.JADE);
+//        } else if (inputStack.getItem() == ModItems.RAW_PYROPE) {
+//            result = new ItemStack(ModItems.PYROPE);
+//        } else {
+//            return; // No valid input, so don't craft anything
+//        }
+//
+//        this.removeStack(INPUT_SLOT, 1);
+//
+//        ItemStack energyStack = getStack(ENERGY_SLOT);
+//        if (energyStack.getItem() == ModItems.ENERGY_INGOT) {
+//            if (energyStack.getDamage() < energyStack.getMaxDamage()) {
+//                energyStack.setDamage(energyStack.getDamage() + 5);
+//            } else {
+//                this.removeStack(ENERGY_SLOT);
+//            }
+//        }
+//
+//        this.setStack(OUTPUT_SLOT, new ItemStack(result.getItem(), getStack(OUTPUT_SLOT).getCount() + result.getCount()));
+//    }
 
     private boolean hasCraftingFinished() {
         return progress >= maxProgress;
@@ -234,36 +229,52 @@ public class GemPolisherBlockEntity extends BlockEntity implements ExtendedScree
     }
 
     private boolean hasRecipe() {
+        Optional<RecipeEntry<GemPolisherRecipe>> recipe = getSlot1Recipe();
+        Optional<RecipeEntry<GemPolisherRecipe>> recipe1 = getSlot2Recipe();
 
-        ItemStack hasInput = getStack(INPUT_SLOT);
-        ItemStack result = null;
-
-        if (hasInput.getItem() == ModItems.RAW_RUBY) {
-            result = new ItemStack(ModItems.RUBY);
-        } else if (hasInput.getItem() == ModItems.RAW_SAPPHIRE) {
-            result = new ItemStack(ModItems.SAPPHIRE);
-        } else if (hasInput.getItem() == ModItems.RAW_GREEN_SAPPHIRE) {
-            result = new ItemStack(ModItems.GREEN_SAPPHIRE);
-        } else if (hasInput.getItem() == ModItems.RAW_BLUE_GARNET) {
-            result = new ItemStack(ModItems.BLUE_GARNET);
-        } else if (hasInput.getItem() == ModItems.RAW_PINK_GARNET) {
-            result = new ItemStack(ModItems.PINK_GARNET);
-        } else if (hasInput.getItem() == ModItems.RAW_GREEN_GARNET) {
-            result = new ItemStack(ModItems.GREEN_GARNET);
-        } else if (hasInput.getItem() == ModItems.RAW_TOPAZ) {
-            result = new ItemStack(ModItems.TOPAZ);
-        } else if (hasInput.getItem() == ModItems.RAW_WHITE_TOPAZ) {
-            result = new ItemStack(ModItems.WHITE_TOPAZ);
-        } else if (hasInput.getItem() == ModItems.RAW_PERIDOT) {
-            result = new ItemStack(ModItems.PERIDOT);
-        } else if (hasInput.getItem() == ModItems.RAW_JADE) {
-            result = new ItemStack(ModItems.JADE);
-        } else if (hasInput.getItem() == ModItems.RAW_PYROPE) {
-            result = new ItemStack(ModItems.PYROPE);
-        }
-
-        return result != null && canInsertAmountIntoOutputSlot(result) && canInsertItemIntoOutputSlot(result.getItem());
+        return recipe.isPresent() && recipe1.isPresent() && canInsertAmountIntoOutputSlot(recipe.get().value().getResult(null))
+                && canInsertItemIntoOutputSlot(recipe.get().value().getResult(null).getItem());
     }
+
+    private Optional<RecipeEntry<GemPolisherRecipe>> getSlot2Recipe() {
+        return this.matchGetter.getFirstMatch(new SingleStackRecipeInput(this.getStack(ENERGY_SLOT)), this.world);
+    }
+
+    private Optional<RecipeEntry<GemPolisherRecipe>> getSlot1Recipe() {
+        return this.matchGetter.getFirstMatch(new SingleStackRecipeInput(this.getStack(INPUT_SLOT)), this.world);
+    }
+
+//    private boolean hasRecipe() {
+//
+//        ItemStack hasInput = getStack(INPUT_SLOT);
+//        ItemStack result = null;
+//
+//        if (hasInput.getItem() == ModItems.RAW_RUBY) {
+//            result = new ItemStack(ModItems.RUBY);
+//        } else if (hasInput.getItem() == ModItems.RAW_SAPPHIRE) {
+//            result = new ItemStack(ModItems.SAPPHIRE);
+//        } else if (hasInput.getItem() == ModItems.RAW_GREEN_SAPPHIRE) {
+//            result = new ItemStack(ModItems.GREEN_SAPPHIRE);
+//        } else if (hasInput.getItem() == ModItems.RAW_BLUE_GARNET) {
+//            result = new ItemStack(ModItems.BLUE_GARNET);
+//        } else if (hasInput.getItem() == ModItems.RAW_PINK_GARNET) {
+//            result = new ItemStack(ModItems.PINK_GARNET);
+//        } else if (hasInput.getItem() == ModItems.RAW_GREEN_GARNET) {
+//            result = new ItemStack(ModItems.GREEN_GARNET);
+//        } else if (hasInput.getItem() == ModItems.RAW_TOPAZ) {
+//            result = new ItemStack(ModItems.TOPAZ);
+//        } else if (hasInput.getItem() == ModItems.RAW_WHITE_TOPAZ) {
+//            result = new ItemStack(ModItems.WHITE_TOPAZ);
+//        } else if (hasInput.getItem() == ModItems.RAW_PERIDOT) {
+//            result = new ItemStack(ModItems.PERIDOT);
+//        } else if (hasInput.getItem() == ModItems.RAW_JADE) {
+//            result = new ItemStack(ModItems.JADE);
+//        } else if (hasInput.getItem() == ModItems.RAW_PYROPE) {
+//            result = new ItemStack(ModItems.PYROPE);
+//        }
+//
+//        return result != null && canInsertAmountIntoOutputSlot(result) && canInsertItemIntoOutputSlot(result.getItem());
+//    }
 
     private boolean canInsertItemIntoOutputSlot(Item item) {
         return this.getStack(OUTPUT_SLOT).getItem() == item || this.getStack(OUTPUT_SLOT).isEmpty();
--- src/main/java/net/moreores/recipe/GemPolisherRecipe.java
+++ src/main/java/net/moreores/recipe/GemPolisherRecipe.java
@@ -1,143 +1,97 @@
-package net.moreores.recipe;//package net.moreores.recipe;
-//
-//import com.mojang.serialization.Codec;
-//import com.mojang.serialization.DataResult;
-//import com.mojang.serialization.MapCodec;
-//import com.mojang.serialization.codecs.RecordCodecBuilder;
-//import net.minecraft.inventory.SimpleInventory;
-//import net.minecraft.item.ItemStack;
-//import net.minecraft.network.RegistryByteBuf;
-//import net.minecraft.network.codec.PacketCodec;
-//import net.minecraft.recipe.Ingredient;
-//import net.minecraft.recipe.Recipe;
-//import net.minecraft.recipe.RecipeSerializer;
-//import net.minecraft.recipe.RecipeType;
-//import net.minecraft.recipe.input.SingleStackRecipeInput;
-//import net.minecraft.registry.RegistryWrapper;
-//import net.minecraft.util.collection.DefaultedList;
-//import net.minecraft.world.World;
-//import net.moreores.block.entity.GemPolisherBlockEntity;
-//
-//import java.util.List;
-//
-//public class GemPolisherRecipe implements Recipe<SingleStackRecipeInput> {
-//    private final ItemStack output;
-//    private final List<Ingredient> recipeItem;
-//    private final Ingredient energySource;
-//
-//    public GemPolisherRecipe(List<Ingredient> ingredient, Ingredient energySource, ItemStack result) {
-//        this.output = result;
-//        this.recipeItem = ingredient;
-//        this.energySource = energySource;
-//    }
-//
-//    public static final RecipeType<GemPolisherRecipe> TYPE = RecipeType.register(Type.ID);
-//
-//    private SingleStackRecipeInput convertToSingleStackRecipeInput(SimpleInventory inventory) {
-//        return new SingleStackRecipeInput(inventory.getStack(GemPolisherBlockEntity.INPUT_SLOT));
-//    }
-//
-//    public DefaultedList<Ingredient> getEnergySource() {
-//        return DefaultedList.of();
-//    }
-//
-//    @Override
-//    public boolean matches(SingleStackRecipeInput input, World world) {
-//        if (world.isClient) {
-//            return false;
-//        }
-//        SimpleInventory inventory = new SimpleInventory();
-//        return recipeItem.get(0).test(inventory.getStack(0));
-//    }
-//
-//    public boolean matches(SimpleInventory inventory, World world) {
-//        SingleStackRecipeInput input = convertToSingleStackRecipeInput(inventory);
-//
-//        return matches(input, world);
-//    }
-//
-//    @Override
-//    public ItemStack craft(SingleStackRecipeInput input, RegistryWrapper.WrapperLookup lookup) {
-//        return output;
-//    }
-//
-//    @Override
-//    public boolean fits(int width, int height) {
-//        return true;
-//    }
-//
-//    @Override
-//    public ItemStack getResult(RegistryWrapper.WrapperLookup registriesLookup) {
-//        return output;
-//    }
-//
-//
-//
-//    @Override
-//    public RecipeSerializer<?> getSerializer() {
-//        return Serializer.INSTANCE;
-//    }
-//
-//    @Override
-//    public RecipeType<?> getType() {
-//        return Type.INSTANCE;
-//    }
-//
-//    public static class Type implements RecipeType<GemPolisherRecipe> {
-//        public static final Type INSTANCE = new Type();
-//        public static final String ID = "gem_polishing";
-//    }
-//
-//    public static class Serializer implements RecipeSerializer<GemPolisherRecipe> {
-//
-//        public static final Serializer INSTANCE = new Serializer();
-//        public static final String ID = "gem_polishing";
-//
-//        public static final MapCodec<GemPolisherRecipe> CODEC = RecordCodecBuilder.mapCodec(ins -> ins.group(
-//                validateAmount(Ingredient.DISALLOW_EMPTY_CODEC, 9).fieldOf("ingredients").forGetter(GemPolisherRecipe::getIngredients),
-//                Ingredient.DISALLOW_EMPTY_CODEC.fieldOf("energySource").forGetter(r -> r.energySource),
-//                ItemStack.VALIDATED_CODEC.fieldOf("result").forGetter(r -> r.output)
-//                ).apply(ins, GemPolisherRecipe::new));
-//
-//
-//        private static Codec<List<Ingredient>> validateAmount(Codec<Ingredient> delegate, int max) {
-//            return delegate.listOf()
-//                    .validate(list -> list.size() > max ? DataResult.error(() -> "Recipe has too many ingredients!") :
-//                            DataResult.success(list)).validate(list -> list.isEmpty() ? DataResult.error(() -> "Recipe has no ingredients!") :
-//                            DataResult.success(list));
-//        }
-//
-//        @Override
-//        public MapCodec<GemPolisherRecipe> codec() {
-//            return CODEC;
-//        }
-//
-//        @Override
-//        public PacketCodec<RegistryByteBuf, GemPolisherRecipe> packetCodec() {
-//            return PacketCodec.ofStatic(Serializer::write, Serializer::read);
-//        }
-//
-//        private static GemPolisherRecipe read(RegistryByteBuf buf) {
-//            int i = buf.readVarInt();
-//            DefaultedList<Ingredient> defaultedList = DefaultedList.ofSize(i, Ingredient.EMPTY);
-//            defaultedList.replaceAll(empty -> Ingredient.PACKET_CODEC.decode(buf));
-//            Ingredient energy = Ingredient.PACKET_CODEC.decode(buf);
-//            ItemStack itemStack = ItemStack.PACKET_CODEC.decode(buf);
-//            return new GemPolisherRecipe(defaultedList, energy, itemStack);
-//        }
-//
-//        private static void write(RegistryByteBuf buf, GemPolisherRecipe recipe) {
-//
-//            buf.writeString(recipe.getGroup());
-//            buf.writeVarInt(recipe.getIngredients().size());
-//
-//            for (Ingredient ingredient : recipe.getIngredients()) {
-//                Ingredient.PACKET_CODEC.encode(buf, ingredient);
-//            }
-//            Ingredient.PACKET_CODEC.encode(buf, recipe.energySource);
-//            ItemStack.PACKET_CODEC.encode(buf, recipe.getResult(null));
-//        }
-//
-//    }
-//
-//}
+package net.moreores.recipe;
+
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.RecipeType;
+import net.minecraft.recipe.input.SingleStackRecipeInput;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.world.World;
+import net.moreores.block.ModBlocks;
+
+public class GemPolisherRecipe implements Recipe<SingleStackRecipeInput> {
+    private final ItemStack output;
+    private final Ingredient ingredient;
+
+    public GemPolisherRecipe(Ingredient ingredient, ItemStack result) {
+        this.output = result;
+        this.ingredient = ingredient;
+    }
+
+    public boolean matches(SingleStackRecipeInput input, World world) {
+        if (world.isClient) return false;
+        return ingredient.test(input.item());
+    }
+
+    @Override
+    public ItemStack craft(SingleStackRecipeInput input, RegistryWrapper.WrapperLookup lookup) {
+        return output.copy();
+    }
+
+    @Override
+    public ItemStack createIcon() {
+        return new ItemStack(ModBlocks.GEM_POLISHER_BLOCK);
+    }
+
+    @Override
+    public boolean fits(int width, int height) {
+        return true;
+    }
+
+    @Override
+    public ItemStack getResult(RegistryWrapper.WrapperLookup registriesLookup) {
+        return output;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return Serializer.INSTANCE;
+    }
+
+    @Override
+    public RecipeType<?> getType() {
+        return Type.INSTANCE;
+    }
+
+    public static class Type implements RecipeType<GemPolisherRecipe> {
+        public static final Type INSTANCE = new Type();
+        public static final String ID = "gem_polishing";
+    }
+
+    public static class Serializer implements RecipeSerializer<GemPolisherRecipe> {
+
+        public static final Serializer INSTANCE = new Serializer();
+        public static final String ID = "gem_polishing";
+
+        public static final MapCodec<GemPolisherRecipe> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
+                Ingredient.DISALLOW_EMPTY_CODEC.fieldOf("ingredient").forGetter(r -> r.ingredient),
+                ItemStack.VALIDATED_CODEC.fieldOf("result").forGetter(r -> r.output)
+        ).apply(instance, GemPolisherRecipe::new));
+
+        @Override
+        public MapCodec<GemPolisherRecipe> codec() {
+            return CODEC;
+        }
+
+        @Override
+        public PacketCodec<RegistryByteBuf, GemPolisherRecipe> packetCodec() {
+            return PacketCodec.ofStatic(Serializer::write, Serializer::read);
+        }
+
+        private static void write(RegistryByteBuf buf, GemPolisherRecipe recipe) {
+            Ingredient.PACKET_CODEC.encode(buf, recipe.ingredient);
+            ItemStack.PACKET_CODEC.encode(buf, recipe.getResult(null));
+        }
+
+        private static GemPolisherRecipe read(RegistryByteBuf buf) {
+            Ingredient ingredient = Ingredient.PACKET_CODEC.decode(buf);
+            ItemStack result = ItemStack.PACKET_CODEC.decode(buf);
+            return new GemPolisherRecipe(ingredient, result);
+        }
+    }
+}
