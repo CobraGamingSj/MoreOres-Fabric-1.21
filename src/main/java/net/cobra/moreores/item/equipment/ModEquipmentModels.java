2fc147711f9a34b7a449247e8ed7916ff84a1736
CobraGamingSj <shashikaanthjain.1983@gmail.com>
2024-12-05 19:00:25 +0530

    WJC

--- src/main/java/net/cobra/moreores/item/equipment/ModEquipmentModels.java
+++
@@ -1,9 +0,0 @@
-package net.cobra.moreores.item.equipment;
-
-import net.cobra.moreores.MoreOresModInitializer;
-import net.minecraft.util.Identifier;
-
-public interface ModEquipmentModels {
-    Identifier RUBY = Identifier.of(MoreOresModInitializer.MOD_ID, "ruby");
-    Identifier SAPPHIRE = Identifier.of(MoreOresModInitializer.MOD_ID, "sapphire");
-}
