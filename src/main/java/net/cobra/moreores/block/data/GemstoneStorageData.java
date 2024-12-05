bc671920d71f525bc6ab5abdb60cda35f5a9cba8
CobraGamingSj <shashikaanthjain.1983@gmail.com>
2024-12-05 18:48:42 +0530

    Removed useless files

--- src/main/java/net/cobra/moreores/block/data/GemstoneStorageData.java
+++
@@ -1,9 +0,0 @@
-//package net.cobra.moreores.block.data;
-//
-//import net.minecraft.network.RegistryByteBuf;
-//import net.minecraft.network.codec.PacketCodec;
-//import net.minecraft.util.math.BlockPos;
-//
-//public record GemstoneStorageData(BlockPos blockPos) {
-//    public static final PacketCodec<RegistryByteBuf, GemstoneStorageData> PACKET_CODEC = PacketCodec.tuple(BlockPos.PACKET_CODEC, GemstoneStorageData::blockPos, GemstoneStorageData::new);
-//}
